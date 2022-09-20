e13923f6295541cd9df80cef6937e65f2ef40223
Toghrul V. Guluzada <toghrulvg@code.local>
2022-09-20 18:54:48 +0400

    delete icon and button disable added

--- assets/script.js
+++ assets/script.js
@@ -3,35 +3,47 @@ let addButton = document.querySelector(".btn");
 let delButton = document.querySelector(".btn1");
 
 
-async function getAll(){
+
+
+async function getAll() {
     let product = await fetch('https://jsonplaceholder.typicode.com/posts')
     let result = await product.json();
     result.forEach(element => {
+
         table.innerHTML += `<td>${element.userId}</td>
         <td>${element.id}</td>
         <td>${element.title}</td>
-        <td>${element.body}</td>`
+        <td>${element.body}</td>
+        <td><span class="delete" style="cursor: pointer">X</span></td>
+        `
+        addButton.classList.add("disabled");
+        let spans = document.querySelectorAll(".delete");
+        spans.forEach(span => {
+            span.addEventListener("click", function () {
+                this.parentNode.parentNode.remove();
+            })
+        });
+
     });
 }
-function delAll(){
+function delAll() {
     table.innerHTML = `<tr>
     <th scope="col">userId</th>
     <th scope="col">Id</th>
     <th scope="col">Title</th>
-    <th scope="col">Antaklaska</th>
+    <th scope="col">Body</th>
   </tr>`
+    addButton.classList.remove("disabled")
 }
 
-function addToTable(){
-    addButton.addEventListener("click",getAll)
+function addToTable() {
+    addButton.addEventListener("click", getAll)
 }
-function deleteFromTable(){
-    delButton.addEventListener("click",delAll)
+function deleteFromTable() {
+    delButton.addEventListener("click", delAll)
 }
-
-
-
 
 
 addToTable();
 deleteFromTable();
+
